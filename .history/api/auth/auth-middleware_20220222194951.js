076d81a5d8ac920345ce0ce38e040b073fe231af
lomelo_x <91914856+lomelo-x@users.noreply.github.com>
2022-02-22 19:52:01 +0100

    validateRoleName MD complete

--- .history/api/auth/auth-middleware_20220222194951.js
+++ .history/api/auth/auth-middleware_20220222194951.js
@@ -1,7 +1,7 @@
-const { JWT_SECRET } = require('../secrets'); // use this secret!
+const { JWT_SECRET } = require("../secrets"); // use this secret!
 
 const restricted = (req, res, next) => {
-	/*
+  /*
     If the user does not provide a token in the Authorization header:
     status 401
     {
@@ -16,11 +16,11 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
-	next();
-};
+  next()
+}
 
-const only = (role_name) => (req, res, next) => {
-	/*
+const only = role_name => (req, res, next) => {
+  /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
     status 403
@@ -30,22 +30,24 @@ const only = (role_name) => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
-	next();
-};
+    next()
+}
+
 
 const checkUsernameExists = (req, res, next) => {
-	/*
+  /*
     If the username in req.body does NOT exist in the database
     status 401
     {
       "message": "Invalid credentials"
     }
   */
-	next();
-};
+  next()
+}
+
 
 const validateRoleName = (req, res, next) => {
-	/*
+  /*
     If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
 
     If role_name is missing from req.body, or if after trimming it is just an empty string,
@@ -63,21 +65,18 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
-	if (!req.body.role_name || !req.body.role_name.trim()) {
-		req.role_name = 'student';
-		next();
-	} else if (req.body.role_name.trim() === 'admin') {
-		next({ status: 422, message: 'Role name can not be admin' });
-	} else if (req.body.role_name.trim().length > 32) {
-		next({ status: 422, message: 'Role name can not be longer than 32 chars' });
-	} else {
-		next();
-	}
-};
+    if (!req.body.role_name || !req.body.role_name.trim()) {
+      req.role_name = 'student'
+      next()
+    } else if (req.body.role_name.trim() === 'admin') {
+      next({ status: 422, message: ''})
+    }
+  next()
+}
 
 module.exports = {
-	restricted,
-	checkUsernameExists,
-	validateRoleName,
-	only,
-};
+  restricted,
+  checkUsernameExists,
+  validateRoleName,
+  only,
+}
